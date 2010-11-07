3896e5a2e46fc57deeeffc6933cd49311acdffa0
mitch0 <mitch0@localhost>
2010-11-07 02:54:12 +0100

    fix copy-waste errors

--- localizations/deDE.lua
+++ localizations/deDE.lua
@@ -1,8 +1,4 @@
--- This file is automagically generated.
--- Please visit http://www.wowace.com/projects/bazooka/localization/
--- if you wish to help with the translation.
-
-local L = LibStub("AceLocale-3.0"):NewLocale("Bazooka", "deDE")
+local L = LibStub("AceLocale-3.0"):NewLocale("Broker_Compass", "deDE")
 if not L then return end
 
 --@localization(locale="deDE", format="lua_additive_table", handle-subnamespaces="none", namespace="", handle-unlocalized="ignore")@
--- localizations/zhTW.lua
+++ localizations/zhTW.lua
@@ -1,8 +1,4 @@
--- This file is automagically generated.
--- Please visit http://www.wowace.com/projects/bazooka/localization/
--- if you wish to help with the translation.
-
-local L = LibStub("AceLocale-3.0"):NewLocale("Bazooka", "zhTW")
+local L = LibStub("AceLocale-3.0"):NewLocale("Broker_Compass", "zhTW")
 if not L then return end
 
 --@localization(locale="zhTW", format="lua_additive_table", handle-subnamespaces="none", namespace="", handle-unlocalized="ignore")@
