ba4d8bff85eb1b19d90df87101fb44474d2399a6
mitch0 <mitch0@localhost>
2010-11-09 23:42:09 +0000

    add original arrow plus one more, allow changing icon by Alt + Left-click

--- Broker_Compass.lua
+++ Broker_Compass.lua
@@ -20,7 +20,7 @@ Broker_CompassDB = Broker_CompassDB or {
 }
 local db
 
-local directions = {
+local Directions = {
     L["NorthEast"],
     L["East"],
     L["SouthEast"],
@@ -30,9 +30,9 @@ local directions = {
     L["NorthWest"],
     L["North"],
 }
-directions[0] = directions[8]
+Directions[0] = Directions[8]
 
-local longDirections = {
+local LongDirections = {
     L["NorthEast-long"],
     L["East-long"],
     L["SouthEast-long"],
@@ -42,7 +42,13 @@ local longDirections = {
     L["NorthWest-long"],
     L["North-long"],
 }
-longDirections[0] = longDirections[8]
+LongDirections[0] = LongDirections[8]
+
+local Icons = {
+    Icon,
+    [[Interface\AddOns\]] .. AppName .. [[\arrow2.tga]],
+    [[Interface\AddOns\]] .. AppName .. [[\arrow3.tga]],
+}
 
 local texcoords = setmetatable({}, {
     __index = function(t, deg)
@@ -59,8 +65,8 @@ local texcoords = setmetatable({}, {
     end
 })
 
-local update -- forward decl
-local bc = {
+local update, bc -- forward decl
+bc = {
         type = "data source",
         label = L["Direction"],
         icon = Icon,
@@ -74,8 +80,23 @@ local bc = {
         end,
         OnClick = function(frame, button)
             if button == "LeftButton" then
-                db.longFormat = not db.longFormat
-                update(true)
+                if IsAltKeyDown() then
+                    if not db.iconIdx or db.iconIdx < 1 or db.iconIdx > #Icons then
+                        db.iconIdx = 2
+                    else
+                        db.iconIdx = db.iconIdx + 1
+                    end
+                    if db.iconIdx > #Icons then
+                        db.iconIdx = nil
+                        bc.icon = Icon
+                    else
+                        bc.icon = Icons[db.iconIdx]
+                    end
+                    update(true)
+                else
+                    db.longFormat = not db.longFormat or nil
+                    update(true)
+                end
             end
         end,
 }
@@ -91,7 +112,7 @@ update = function(forced)
     end
     local idx = round(deg / 45)
     bc.value = deg
-    bc.text = db.longFormat and longDirections[idx] or directions[idx]
+    bc.text = db.longFormat and LongDirections[idx] or Directions[idx]
     bc.iconCoords = texcoords[deg]
 end
 
@@ -99,6 +120,9 @@ local bcTimerFrame = CreateFrame("Frame")
 bcTimerFrame:SetScript("OnEvent", function(frame, event, arg)
     if event == 'ADDON_LOADED' and arg == AppName then
         db = Broker_CompassDB
+        if db.iconIdx and 1 <= db.iconIdx and db.iconIdx <= #Icons then
+            bc.icon = Icons[db.iconIdx]
+        end
         update(true)
         local bcTimerAnim = frame:CreateAnimationGroup()
         local anim = bcTimerAnim:CreateAnimation()
